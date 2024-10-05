414b0debc046ac438a5d8ba210d2d4898733eb46
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-10-05 19:46:11 +0530

    new

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
         ...(isWorker && { workAddress }),
         isWorker
       };
-      const response = await axios.post(`https://${import.meta.env.VITE_API_URL}/api/signup`, userData);
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/signup`, userData);
       login(response.data);
       navigate('/');
     } catch (err) {
